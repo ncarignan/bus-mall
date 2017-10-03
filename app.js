9f3e68c0abcf8a461e774d73ddf5aee3c6d7a89f
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-10-03 10:44:25 -0700

    fixed it all, just needs css

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ function Product(name, filepath){
   Product.allProducts.push(this);
 }
 Product.allProducts = [];
+Product.productPickHistory = [[]];
+Product.lastPicked = [];
 
 //rand display pictures
 
@@ -19,9 +21,9 @@ var imgEl1 = document.getElementById('productPic1');
 var imgEl2 = document.getElementById('productPic2');
 var imgEl3 = document.getElementById('productPic3');
 
-imgEl1.addEventListener('click', randomProduct);
-imgEl2.addEventListener('click', randomProduct);
-imgEl3.addEventListener('click', randomProduct);
+imgEl1.addEventListener('click', function(){randomProduct();productChoiceLogger(1);});
+imgEl2.addEventListener('click', function(){randomProduct();productChoiceLogger(2);});
+imgEl3.addEventListener('click', function(){randomProduct();productChoiceLogger(3);});
 
 
 //new instances
@@ -29,62 +31,79 @@ imgEl3.addEventListener('click', randomProduct);
 new Product('R2D2 Bag', 'img/bag.jpg');
 new Product('Banana Slicer', 'img/banana.jpg');
 new Product('Toilet Paper Ipad', 'img/bathroom.jpg');
-new Product('Banana Slicer', 'img/boots.jpg');
-new Product('R2D2 Bag', 'img/breakfast.jpg');
-new Product('Banana Slicer', 'img/bubblegum.jpg');
-new Product('R2D2 Bag', 'img/chair.jpg');
-new Product('Banana Slicer', 'img/cthulhu.jpg');
-new Product('R2D2 Bag', 'img/dog-duck.jpg');
-new Product('Banana Slicer', 'img/dragon.jpg');
-new Product('R2D2 Bag', 'img/pen.jpg');
-new Product('Banana Slicer', 'img/pet-sweep.jpg');
-new Product('R2D2 Bag', 'img/scissors.jpg');
-new Product('Banana Slicer', 'img/shark.jpg');
-new Product('R2D2 Bag', 'img/sweep.png');
-new Product('Banana Slicer', 'img/tauntaun.jpg');
-new Product('R2D2 Bag', 'img/unicorn.jpg');
-new Product('Banana Slicer', 'img/usb.gif');
-new Product('R2D2 Bag', 'img/water-can.jpg');
-new Product('Banana Slicer', 'img/wine-glass.jpg');
+new Product('Toeless Boots', 'img/boots.jpg');
+new Product('Coffee Toaster', 'img/breakfast.jpg');
+new Product('Meatball Bubblegum', 'img/bubblegum.jpg');
+new Product('butt bumper', 'img/chair.jpg');
+new Product('cthulhu', 'img/cthulhu.jpg');
+new Product('dog duck', 'img/dog-duck.jpg');
+new Product('dragon meat', 'img/dragon.jpg');
+new Product('pentencile', 'img/pen.jpg');
+new Product('pet swiffer', 'img/pet-sweep.jpg');
+new Product('pizza scizzors', 'img/scissors.jpg');
+new Product('shark bag', 'img/shark.jpg');
+new Product('baby swiffer', 'img/sweep.png');
+new Product('tauntaun bag', 'img/tauntaun.jpg');
+new Product('tentacle meat', 'img/unicorn.jpg');
+new Product('naughty arm', 'img/usb.gif');
+new Product('water can', 'img/water-can.jpg');
+new Product('wine glass', 'img/wine-glass.jpg');
 //etc
 
-var productPickHistory = [[],[],[],[],[],[]];
-var lastPicked = [];
 
 function randomProduct(){
-  lastPicked = [];
-
+  Product.lastPicked = [];
   for(var i = 0; i < 3; i++){
-
     var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-
-    if (lastPicked.includes(randomIndex) || productPickHistory[productPickHistory.length - 1].includes(randomIndex) ){
+    if (Product.lastPicked.includes(randomIndex) || Product.productPickHistory[Product.productPickHistory.length - 1].includes(randomIndex) ){
       i--;
-      console.log('already picked ' + randomIndex);
+      // console.log('already picked ' + randomIndex);
     }else{
-      lastPicked.push(randomIndex);
-      console.log('a picture gets ' + lastPicked);
-      imgEl1.src = Product.allProducts[randomIndex].filepath;
-    //   if (i = 0){
-    //   }else if(i = 1){
-    //     imgEl2.src = Product.allProducts[randomIndex].filepath;
-    //   }else if(i = 2){
-    //     imgEl3.src = Product.allProducts[randomIndex].filepath;
-    //   }
-    // }
+      Product.lastPicked.push(randomIndex);
+      // console.log('a picture gets ' + Product.lastPicked);
+      // console.log(i);
+      if (i === 0){
+        imgEl1.src = Product.allProducts[randomIndex].filepath;
+        Product.allProducts[randomIndex].count++;
+        // console.log(Product.allProducts[randomIndex].count);
+      }else if(i === 1){
+        imgEl2.src = Product.allProducts[randomIndex].filepath;
+        Product.allProducts[randomIndex].count++;
+      }else if(i === 2){
+        imgEl3.src = Product.allProducts[randomIndex].filepath;
+        Product.allProducts[randomIndex].count++;
+      }
     }
-  }productPickHistory.push(lastPicked);
-  // productPickHistory=productPickHistory.slice(4:);
-  console.log('prodpickhist' + productPickHistory);
+
+  }Product.productPickHistory.push(Product.lastPicked);
+  // Product.productPickHistory=Product.productPickHistory.slice(4:);
+  // console.log('prodpickhist' + Product.productPickHistory);
 }
-//   var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-//   imgEl1.src = Product.allProducts[randomIndex].filepath;
-//   randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-//   imgEl2.src = Product.allProducts[randomIndex].filepath;
-//   randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-//   imgEl3.src = Product.allProducts[randomIndex].filepath;
-//
-// }
+
+function productChoiceLogger(imgElNumber){
+  if (imgElNumber == 1){
+    Product.allProducts[(Product.lastPicked[0])].picked++;
+  }else if (imgElNumber === 2) {
+    Product.allProducts[Product.lastPicked[1]].picked++;
+  }else if (imgElNumber === 3){
+    Product.allProducts[ Product.lastPicked[2] ].picked++;
+  }
+  if (Product.productPickHistory.length > 5){
+    imgEl1.removeEventListener('click', function(){randomProduct();productChoiceLogger(1);});
+    imgEl2.removeEventListener('click', function(){randomProduct();productChoiceLogger(2);});
+    imgEl3.removeEventListener('click', function(){randomProduct();productChoiceLogger(3);});
+
+    var ulEl = document.getElementById('clickStats');
+    for(var i in Product.allProducts){
+      var liEl = document.createElement('li');
+      liEl.textContent = Product.allProducts[i].name + ' was displayed ' + Product.allProducts[i].count + ' times and was picked ' + Product.allProducts[i].picked + ' times';
+      ulEl.appendChild(liEl);
+
+    }
+  }
+}
+
+
 
 
 
